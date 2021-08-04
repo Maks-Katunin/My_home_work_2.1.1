1b3e9d44c62764cb650d230b20cf7c5e862c4162
Maks-Katunin <katunin87@gmail.com>
2021-08-05 04:05:14 +0600

    очень устал

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int aa = 20; // здесь будет делаться случайное число
-        int ba = 45;
+        int aa = 0; // здесь будет делаться случайное число
+        int ba = 100;
         int ageRandom = aa + (int) (Math.random() * ((ba - aa) + 1));
-        
 
-        int at = 20; // здесь будет делаться случайное число
+
+        int at = -20; // здесь будет делаться случайное число
         int bt = 45;
         int tempRandom = at + (int) (Math.random() * ((bt - at) + 1));
 
@@ -17,22 +17,24 @@ public class Main {
         test("VOZRAST ", ageRandom, "TEMPERATURA ", tempRandom);
 
 
-        int resultatIzTogoCamogoMetoda = totSamiyMetod(ageRandom, tempRandom);
-        //возвращаемый метод с входящими параметрами
-        System.out.println(resultatIzTogoCamogoMetoda + " izMetoda");
+
 
     }
 
-    public static int totSamiyMetod(int ageRandom, int tempRandom) {
-
-        int result = ageRandom + tempRandom;
-        return result;
-
-    }
 
     public static void test(String word, int vozrast, String word1, int temperature) {
         System.out.println(word + vozrast);
         System.out.println(word1 + temperature);
+        int age = vozrast;
+        int temp = temperature;
+
+        if ((age >= 20 && age <= 45) && (temp >= -2 && temp <= 30))
+            System.out.println("go to walk");
+        else if (age < 20 && temp >= 0 && temp <= 28)
+            System.out.println("go to walk");
+        else if (age >= 45 && temp >= -10 && temp <= 25)
+            System.out.println("go to walk");
+        else System.out.println("stay at home");
     }
 
 }
